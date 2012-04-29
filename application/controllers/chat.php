cb8214aa9b45fdb4f99dcd994144638494d97b0c
Priyadarshi Lahiri <me@priyolahiri.co.cc>
2012-04-29 17:01:50 +0530

    fix

--- application/controllers/chat.php
+++ application/controllers/chat.php
@@ -5,7 +5,7 @@ Class Chat_Controller extends Base_Controller {
 		$error = Session::get('error');
 		$user = new User;
 		if (!$user->authstatus) {
-			return View::make('error.other')->with('title', "Authentication Error")->with('error', "You need to be signed in to create a chat.");
+			return View::make('error.other')->with('title', "Authentication Error")->with('error', "You need to be signed in to create a chat.")->with('user', $user);
 		}
 		return View::make('chat.new')->with('regform', $regform)->with('error', $error)->with('success', $success)->with('user', $user);
 	}
