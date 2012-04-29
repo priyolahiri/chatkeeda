a0f9ad5e4767246b9229659bfcf76ba0465c57d0
Priyadarshi Lahiri <me@priyolahiri.co.cc>
2012-04-29 23:18:45 +0530

    added pusher and underscore to chatnow page

--- application/views/chat/now.php
+++ application/views/chat/now.php
@@ -5,6 +5,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 		<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="/js/underscore.min.js"></script>
+		<script src="http://js.pusher.com/1.11/pusher.min.js" type="text/javascript"></script>
 		<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" media="all" />
 		<link rel="stylesheet" type="text/css" href="/css/font-awesome.css" media="all" />
 		<link rel="stylesheet" type="text/css" href="/css/custom.css" media="all" />
