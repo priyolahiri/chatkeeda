e8b2241670b63e2b420cb37411c5bba3121a6036
Priyadarshi Lahiri <me@priyolahiri.co.cc>
2012-05-01 20:36:55 +0530

    fix

--- application/routes.php
+++ application/routes.php
@@ -133,7 +133,7 @@ Route::post('chataction/(:any)/(:any)', function($slug, $action) {
 	if ($action == "sendscore") {
 		if ($chatauth['admin'] or $chatauth['superadmin']) {
 			$score = Input::get('score_text');
-			if (!$score or $score = "") {
+			if (!$score or $score == "") {
 				return json_encode(array("success" => false, "msg" => "Score cannot be empty!"));
 			}
 			$chat->setScore($score);
